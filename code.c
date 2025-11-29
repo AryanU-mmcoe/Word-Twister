f0ce049e559786cd125b1c2174ef919585024ff3
Aryan U <aryanunhale2025.comp@mmcoe.edu.in>
2025-11-30 01:09:52 +0530

    Update code.c

--- code.c
+++ code.c
@@ -5,15 +5,21 @@ int main() {
     char pass[30];
     int length, special = 0;
     
-
     printf("Enter your password: ");
     scanf("%s", pass);
 
-    length = strlen(pass);
+    int i = 0;
+    while (pass[i] != '\0') {
 
-    if (strchr(pass, '!') || strchr(pass, '@') || strchr(pass, '#') ||
-        strchr(pass, '$') || strchr(pass, '%') || strchr(pass, '&') || strchr(pass, '*')) {
-        special = 1;
+        length++;
+
+        if (pass[i] == '!' || pass[i] == '@' || pass[i] == '#' ||
+            pass[i] == '$' || pass[i] == '%' || pass[i] == '&' ||
+            pass[i] == '*') {
+            special = 1;
+        }
+
+        i++;
     }
 
     printf("Password length: %d\n", length);
